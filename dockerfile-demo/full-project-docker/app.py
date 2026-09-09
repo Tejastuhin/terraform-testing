669b116c6055e4a5cc1b2bbf081cd318c0190d68
Tejastuhin <tejas.tuhin@gmail.com>
2026-09-09 11:45:12 +0530

    Issues fixed yet again

--- dockerfile-demo/full-project-docker/app.py
+++ dockerfile-demo/full-project-docker/app.py
@@ -1,23 +1,27 @@
 import os
 import psycopg
 from flask import Flask
+
 app = Flask(__name__)
 
-connection = psycopg.connect(
-    host = os.getenv("DB_HOST"),
-    port=5432,
-    user = os.getenv("POSTGRES_USER"),
-    password = os.getenv("POSTGRES_PASSWORD"),
-    dbname = os.getenv("POSTGRES_DB")
-)
 
 @app.route("/")
 def home():
+    connection = psycopg.connect(
+        host=os.getenv("DB_HOST"),
+        port=5432,
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        dbname=os.getenv("POSTGRES_DB")
+    )
+
     with connection.cursor() as cursor:
-        cursor.execute("SELECT 'Hello from POSTgreSQL'")
+        cursor.execute("SELECT 'Hello from PostgreSQL!'")
         result = cursor.fetchone()
+
+    connection.close()
 
     return result[0]
 
 
-app.run(host = "0.0.0.0", port = 8000)
+app.run(host="0.0.0.0", port=8000)
